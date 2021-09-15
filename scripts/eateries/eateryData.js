1fd457e38dea448a18f41e21064f2cc4cfb80742
Kaitlyn Long <katielong11@gmail.com>
2021-09-15 16:02:16 -0500

    started writing code that will populate saved trip to an aside

--- scripts/eateries/eateryData.js
+++ scripts/eateries/eateryData.js
@@ -1,7 +1,7 @@
 import { eatery } from "./eatery.js";
 
 export const eatList = (eatAll) => {
-    let postHTML = '<select id="eat_info"> <option value="0"> please select Eatery </option> '; 
+    let postHTML = '<select class="dropdown" id="eat_info"> <option value="0"> please select Eatery </option> '; 
 
     for (const eatObject of eatAll) {
         postHTML += eatery(eatObject)
--- scripts/parks/ParkDropDown.js
+++ scripts/parks/ParkDropDown.js
@@ -2,7 +2,7 @@ import { parks } from "./park.js";
 
 export const parklist = (allParks) => {
 
-    let postHTML = '<select id="park_sec"> <option value="0">please select a park</option> '; 
+    let postHTML = '<select class="dropdown" id="park_sec"> <option value="0">please select a park</option> '; 
     console.log(allParks)
     for (const parkObject of allParks) {
         postHTML += parks(parkObject)
